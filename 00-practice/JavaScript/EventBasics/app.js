3bf8ee20f18185f5bffce937097b98ba73d1b8d6
Heracles404 <404heracles@gmail.com>
2022-11-09 16:47:36 +0800

    Random Color Picker v1

--- 00-practice/JavaScript/EventBasics/app.js
+++ 00-practice/JavaScript/EventBasics/app.js
@@ -1,3 +1,4 @@
+// OnClick Property
 const btn = document.getElementById('v2')
 
 btn.onclick = function () {
@@ -15,3 +16,9 @@ btn.onmouseenter = scream;
 document.querySelector('h1').onclick = function () {
     alert('You clicked me!')
 }
+
+// Event Listeners
+const  btn3 = document.querySelector('#v3')
+btn3.addEventListener('dblclick', function (){
+    alert("CLICKED!")
+})
